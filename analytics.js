b031cb7d490779f9fb96083908409ec13ccc1d61
MangaGoRaw <ghazaalbaloch3+a1@gmail.com>
2026-09-17 22:46:27 +0500

    Add Google Analytics tracking for all pages

--- analytics.js
+++ analytics.js
@@ -1,1 +1,18 @@
-window.mangaGoRawAnalytics={pageView:function(){}};
+(function(){'use strict';
+  var ID='G-HC32QHLNXB';
+  if(!window.__mangaGoRawGA){
+    window.__mangaGoRawGA=true;
+    window.dataLayer=window.dataLayer||[];
+    window.gtag=function(){dataLayer.push(arguments)};
+    gtag('js',new Date());
+    gtag('config',ID,{send_page_view:true});
+    var s=document.createElement('script');
+    s.async=true;
+    s.src='https://www.googletagmanager.com/gtag/js?id='+encodeURIComponent(ID);
+    document.head.appendChild(s);
+  }
+  window.mangaGoRawAnalytics={
+    pageView:function(params){if(window.gtag)window.gtag('event','page_view',params||{});},
+    event:function(name,params){if(window.gtag)window.gtag('event',name,params||{});}
+  };
+})();
